2f5deb54bdb407ce0fa15ccfbf654007f61bdd8d
Meg Osato <megosato@dhcp-v118-240.mobile.uci.edu>
2023-06-14 14:07:10 -0700

    wip

--- openff_wrapper.py
+++ openff_wrapper.py
@@ -7,3 +7,11 @@ def create_molecule(mol, ligcode="LIG"):
     for atom in ligand_molecule.atoms:
         atom.metadata["residue_name"] = ligcode
     return ligand_molecule
+
+
+def create_molecule(mol, ligcode="LIG"):
+    ligand_molecule = Molecule.from_mol2(mol)
+    ligand_molecule.name = ligcode
+    for atom in ligand_molecule.atoms:
+        atom.metadata["residue_name"] = ligcode
+    return ligand_molecule
